8c3be7a4f5f5ca36f8e3f3c1e7a929082a43259a
amoghpalnitkar <amogh.1337@gmail.com>
2014-06-17 17:10:30 +0530

    Added comments

--- scrapper.py
+++ scrapper.py
@@ -6,11 +6,20 @@ BASE_URL = "http://www.imdb.com"
 movie_rating = 0
 movie_description = ""
 
+#
+#   Print the details of the found movie
+#
+
 def print_details(movie_description,movie_rating):
     print "Movie Name : " + movie
     print "Movie Rating : " + movie_rating
     print "Movie Description : " + movie_description
     return
+
+#
+#   Get the details of the found movie. Scrape throu' for director and rating.
+#   More details to be added.
+#
 
 def get_details(link):
     page = urlopen(link)
@@ -20,26 +29,22 @@ def get_details(link):
     div = soup.find('p', itemprop="description")
     movie_description = div.contents[0] 
     DivDirector = soup.find('div', itemprop="director")
-    #print DivDirector
     director = DivDirector.find('span', itemprop="name")
-#director = DivDirector.find('span', itemprop="name")
     movie_director = director.contents[0]
     print_details(movie_description,movie_rating)
     return
+
+#
+#   Find the movie, using imdb/find? tag. Get the first matched movie.
+#
 
 def get_category_links(movie):
     page = urlopen(BASE_URL +"/find?" + movie)
     soup = BeautifulSoup(page)
     div = soup.find('table', class_="findList")
-#print div
-#print soup
     links = div.find('a', href=True)
-    #print links['href']
-    #print links
-    #href = links.find()
-    #print links.contents
+
     complete_link = BASE_URL + links['href']
-    #print complete_link
     get_details(complete_link)
     return
 def format_movie():
@@ -50,6 +55,5 @@ movie = raw_input("Enter movie name : ")
 #movieCapital = capitalize(movie)
 movie = movie.replace(" ","-")
 #print movie
-#movie = "the-prestige"
 get_category_links(movie)
 
